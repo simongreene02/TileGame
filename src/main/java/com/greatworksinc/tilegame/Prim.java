7dd494d71b4f3d5d74b9ba13f21bea57b022e78f
Simon Green <simongreen02@gmail.com>
2019-04-28 10:41:16 -0500

    Worked on cleaning up Prim code.

--- src/main/java/com/greatworksinc/tilegame/Prim.java
+++ src/main/java/com/greatworksinc/tilegame/Prim.java
@@ -3,27 +3,40 @@ package com.greatworksinc.tilegame;
 import com.google.common.annotations.VisibleForTesting;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 //Taken from http://jonathanzong.com/blog/2012/11/06/maze-generation-with-prims-algorithm
 
 public class Prim {
+  private final int numOfRows;
+  private final int numOfCols;
+
+  private final Random random;
+
 
   public static void main(String[] args) {
+  }
+
+  public Prim(int numOfRows, int numOfCols) {
+    this.numOfRows = numOfRows;
+    this.numOfCols = numOfCols;
+  }
+
+  public void generateMaze() {
     // dimensions of generated maze
-    int r = 11, c = 100;
 
     // build maze and initialize with only walls
-    StringBuilder s = new StringBuilder(c);
-    for (int x = 0; x < c; x++) {
+    StringBuilder s = new StringBuilder(numOfCols);
+    for (int x = 0; x < numOfCols; x++) {
       s.append('+');
     }
-    char[][] maz = new char[r][c];
-    for (int x = 0; x < r; x++) {
+    char[][] maz = new char[numOfRows][numOfCols];
+    for (int x = 0; x < numOfRows; x++) {
       maz[x] = s.toString().toCharArray();
     }
 
     // select random point and open as start node
-    Point st = new Point((int) (Math.random() * r), (int) (Math.random() * c), null);
+    Point st = new Point((random.nextInt(numOfRows)), (random.nextInt(numOfCols)), null);
     maz[st.r][st.c] = 'S';
 
     // iterate through direct neighbors of node
@@ -34,11 +47,11 @@ public class Prim {
           continue;
         }
         try {
-          if (maz[st.r + x][st.c + y] == '-') {
+          if (isPointInList(maz, new Point(st.r + x, st.c + y, null)) && maz[st.r + x][st.c + y] == '-') {
             continue;
           }
         } catch (Exception e) { // ignore ArrayIndexOutOfBounds
-          continue;
+          throw new RuntimeException("TODO: Fill in boundry check", e);
         }
         // add eligible points to frontier
         frontier.add(new Point(st.r + x, st.c + y, st));
@@ -70,7 +83,7 @@ public class Prim {
                   continue;
                 }
                 try {
-                  if (isPointInList(maz, op) && maz[op.r + x][op.c + y] == '-') {
+                  if (isPointInList(maz, new Point(op.r + x, op.c + y, null)) && maz[op.r + x][op.c + y] == '-') {
                     continue;
                   }
                 } catch (Exception e) {
@@ -92,8 +105,8 @@ public class Prim {
     }
 
     // print final maze
-    for (int i = 0; i < r; i++) {
-      for (int j = 0; j < c; j++) {
+    for (int i = 0; i < numOfRows; i++) {
+      for (int j = 0; j < numOfCols; j++) {
         System.out.print(maz[i][j]);
       }
       System.out.println();
@@ -106,9 +119,9 @@ public class Prim {
   }
 
   static class Point {
-    Integer r;
-    Integer c;
-    Point parent;
+    private final Integer r;
+    private final Integer c;
+    private final Point parent;
 
     public Point(int x, int y, Point p) {
       r = x;
